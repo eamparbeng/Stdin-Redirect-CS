825f34b84f360ba0b2de4e9b47b5025040d7d5b4
Bra Kofi <kofi@acsghana.com>
2022-01-28 08:39:23 +0000

    Piping in standard in or redirecting from inputed file as args[0]

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace console_redir_proto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader reader = null;
+
+            if (args.Length == 0 || !File.Exists(args[0]))
+            {
+                if (!Console.IsInputRedirected)
+                {
+                    Console.WriteLine("No pipe!");
+                    return;
+                }
+                reader = Console.In;
+            }
+            else
+            {
+                reader = new StreamReader(args[0]);
+            }
+
+            string line = null;
+            int counter = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine($"{++counter} Processed: {line}");
+            }
+        }
+    }
+}
